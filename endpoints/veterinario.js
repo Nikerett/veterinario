36c2d791aea685cc08ae41b68bb9824eed0e9cf3
hux05 <niusya2004@gmail.com>
2024-04-27 11:26:14 +0200

    tabelle create

---
+++ endpoints/veterinario.js
@@ -0,0 +1,120 @@
+function endpoint(app, connpool) {
+
+    app.post("/api/tasks", (req, res) => {
+        var errors = []
+        /* controllo dati inseriti
+        if (!req.body.description) {
+            errors.push("No description specified");
+        }
+        if (req.body.status === "") {
+            errors.push("No status specified");
+        }
+        */
+        if (errors.length) {
+            res.status(400).json({ "error": errors.join(",") });
+            return;
+        }
+        var data = {
+            description: req.body.description,
+            status: req.body.status,
+        }
+
+        var sql = 'INSERT INTO veterinario (id_vet, nome, cognome, dataN) VALUES (1,gianfry, morelli, 2005-11-24)'
+        var params = [data.description, data.status]
+        connpool.query(sql, params, (error, results) => {
+            if (error) {
+                res.status(400).json({ "error": error.message })
+                return;
+            }
+            res.json({
+                "message": "success",
+                "data": data,
+                "id": this.insertID
+            })
+            console.log(results)
+        });
+
+    })
+
+
+
+    app.get("/api/tasks", (req, res, next) => {
+        var sql = "select * from veterinario"
+        var params = []
+        connpool.query(sql, params, (err, rows) => {
+            if (err) {
+                res.status(400).json({ "error": err.message });
+                return;
+            }
+            res.json({
+                "message": "success",
+                "data": rows
+            })
+        });
+    });
+
+
+    app.get("/api/tasks/:id", (req, res) => {
+        var sql = "select * from veterinario where id_vet = 1"
+        var params = [req.params.id]
+        connpool.query(sql, params, (err, rows) => {
+            if (err) {
+                res.status(400).json({ "error": err.message });
+                return;
+            }
+            res.json({
+                "message": "success",
+                "data": rows[0]
+            })
+        });
+    });
+
+
+    app.put("/api/tasks/:id", (req, res) => {
+        var data = {
+            description: req.body.description,
+            status: req.body.status,
+        }
+        connpool.execute(
+            `UPDATE veterinario set 
+               description = COALESCE(?,description), 
+               status = COALESCE(?,status) 
+               WHERE id_vet =1`,
+            [data.description, data.status, req.params.id],
+            function (err, result) {
+                if (err) {
+                    res.status(400).json({ "error": err.message })
+                    return;
+                }
+                console.log(result)
+                res.json({
+                    message: "success",
+                    data: data,
+                    changes: result.affectedRows
+                })
+            });
+    })
+
+
+
+    app.delete("/api/tasks/:id", (req, res) => {
+        connpool.execute(
+            'DELETE FROM veterinario WHERE id_vet =1',
+            [req.params.id],
+            function (err, result) {
+                if (err) {
+                    res.status(400).json({ "error": err.message })
+                    return;
+                }
+                res.json({ "message": "deleted", changes: result.affectedRows })
+            });
+    })
+
+
+}
+
+
+
+
+
+module.exports = endpoint;
